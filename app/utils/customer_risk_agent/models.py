c554e35fe74ba30937cf8eea4ad325faf7d121cd
JaninduMunasinghe <munasin-ps20057@stu.kln.ac.lk>
2025-03-16 16:11:42 +0530

    added additional validation to the BaseModel

--- app/utils/customer_risk_agent/models.py
+++ app/utils/customer_risk_agent/models.py
@@ -6,12 +6,13 @@ class CustomerData(BaseModel):
     age: int = Field(ge=18, le=100)
     gender: str = Field(pattern="^(Male|Female)$")
     vehicleType: str = Field(pattern="^(Hiring|Private)$")
-    totalClaims: int
+    totalClaims: int = Field(ge=0) 
     reason: str = Field(pattern="^(Driver Fault|3rd Party Fault)$")
-    premium: float
-    claimAmount: float
-    insuredPeriod: int
-
+    premium: float = Field(gt=0)  # Positive premium
+    claimAmount: float = Field(ge=0)  # Non-negative claim amount
+    insuredPeriod: int = Field(gt=0)  # Positive insured period
+    
+    
     @field_validator('age')
     def validate_age(cls, v):
         if v < 18 or v > 100:
