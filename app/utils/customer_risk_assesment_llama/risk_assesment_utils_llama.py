2af12baa2ac62500f24f4c3e047d8a9ece412b99
JaninduMunasinghe <munasin-ps20057@stu.kln.ac.lk>
2025-03-17 23:52:46 +0530

    update yaml file

--- app/utils/customer_risk_assesment_llama/risk_assesment_utils_llama.py
+++ app/utils/customer_risk_assesment_llama/risk_assesment_utils_llama.py
@@ -2,7 +2,7 @@ import os
 from groq import Groq
 
 def get_groq_client():
-    api_key = "sk_1mgo3NgcsgSUaUDebbDsWGdyb3FYJpl6bjuUzbkciQ0RMRaCAgAo"
+    api_key = os.getenv("GROQ_API_KEY")
     if not api_key:
         raise ValueError("GROQ_API_KEY environment variable is not set")
     return Groq()
