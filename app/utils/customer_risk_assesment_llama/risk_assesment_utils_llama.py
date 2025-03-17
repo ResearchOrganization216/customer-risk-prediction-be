24c96bd0703a6ebe06049168f5c78e376ad965d0
JaninduMunasinghe <munasin-ps20057@stu.kln.ac.lk>
2025-03-17 22:51:17 +0530

    fixed groq api key issue

--- app/utils/customer_risk_assesment_llama/risk_assesment_utils_llama.py
+++ app/utils/customer_risk_assesment_llama/risk_assesment_utils_llama.py
@@ -2,7 +2,7 @@ import os
 from groq import Groq
 
 def get_groq_client():
-    api_key = os.getenv("GROQ_API_KEY")
+    api_key = "sk_1mgo3NgcsgSUaUDebbDsWGdyb3FYJpl6bjuUzbkciQ0RMRaCAgAo"
     if not api_key:
         raise ValueError("GROQ_API_KEY environment variable is not set")
     return Groq()
