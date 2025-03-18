de552ee48d6b8c83fa8a4c561cbecffafe495ba1
JaninduMunasinghe <munasin-ps20057@stu.kln.ac.lk>
2025-03-18 19:38:00 +0530

    updated config file

--- config.py
+++ config.py
@@ -2,13 +2,11 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@34.142.174.193:5432/InnoAInsure'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@34.142.175.163:5432/InnoAInsure'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    BUCKET_NAME = "innoainsure-bucket" 
-     # For local development, provide a key file; in production, you can set this to an empty string or None.
-    #KEY_PATH = "C:\Users\Administrator\Documents\keyfiles\\proven-answer-451215-k1-190fdb8da7df.json"
-    KEY_PATH = ""  # Set to an empty string in production
-    PROJECT_ID = "proven-answer-451215-k1"
+    BUCKET_NAME = "innoainsure-bucket"
+    KEY_PATH = "C:\\Users\\Sithija\\Documents\\keyfiles\\innoainsure-project-531bfaa81104.json"  # Set to an empty string in production
+    PROJECT_ID = "innoainsure-project"
     SECRET_KEY = 'your-very-secret-key'
 
     MODEL_PATH = os.path.join('app', 'models', 'xgboost_model.pkl')
